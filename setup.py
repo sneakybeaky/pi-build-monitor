dd1a3dcc9972a322367c4b70789d8c219f1a2bb3
Jon Barber <jon.barber@acm.org>
2015-09-29 10:35:29 +0100

    Initial version

---
+++ setup.py
@@ -0,0 +1,38 @@
+
+'''
+    The proper way to install this package is use pip to install it. If you
+    want to install this local copy, use this command:
+
+        pip install .
+
+    If you don't use pip to install, then 'pip uninstall' won't work as
+    expected.
+'''
+
+from distutils.core import setup
+from setuptools import find_packages
+from os import listdir
+from os.path import dirname, join, isfile, isdir
+import sys
+
+import gocd_parser.version
+
+setup_dir = dirname(__file__)
+package_name = 'uh_build_monitor'
+
+scripts = []
+
+install_requires=open(join(setup_dir, 'requirements.txt')).readlines()
+
+setup(
+    name=package_name,
+    version='0.1',
+    description='Raspberry PI & Unicorn HAT build monitor',
+    author='Jon Barber & Iain Miller',
+    author_email='jon.barber@acm.org',
+    url='https://github.com/sneakybeaky/pi-build-monitor',
+    package_dir={ package_name: 'uh_build_monitor' },
+    packages=find_packages(),
+    install_requires=install_requires,
+    scripts=scripts
+)
---
+++ tests/uh_buildmon_tests.py
@@ -0,0 +1,11 @@
+from nose.tools import *
+import uh_build_monitor
+
+def setup():
+    print "SETUP!"
+
+def teardown():
+    print "TEAR DOWN!"
+
+def test_basic():
+    print "I RAN!"
